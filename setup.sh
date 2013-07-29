43aab767ede64c4c24ac0b988d83b1dc1c5a1939
Andy Warr <andywarr@gmail.com>
2013-07-29 09:28:58 -0700

    Updated setup.sh to use nodejs 10.14, include forever and nodeman

--- setup.sh
+++ setup.sh
@@ -10,8 +10,16 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+nvm install v0.10.14
+nvm use v0.10.14
+
+# Insatll forever to allow nodejs apps to run in the background
+# https://github.com/nodejitsu/forever
+npm install -g forever
+
+# Install nodeman to monitor changes to nodejs files
+# https://github.com/remy/nodemon
+npm install -g nodemon
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
